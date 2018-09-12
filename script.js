2864234d7c70b6b013b79ececbff53a6d386f792
Tong Yang <tyang19@id.iit.edu>
2018-09-12 01:24:59 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var name = "Tong";
+
+var age = 25;
+
+console.log(age*5);
+
+var last_name = "Yang";
+
+console.log(name + " " + last_name);
+
+var fruits = [
+    "Apples",
+    "Oranges",
+    "Pears",
+    "Bananas",
+    "Cherries",
+]
+
+console.log(fruits.length);
+
+console.log(fruits[0]);
+
+var last_item = fruits[fruits.length-1];
+
+console.log(last_item);
+
+var animal_a = {
+    type:"cat",
+    age:"6",
+    name:"Fry",
+    speak:"Meow!",
+}
+
+console.log(animal_a);
+
+var animal_b = {
+    type:"dog",
+    age:"3",
+    name:"Mabel",
+    speak:"Wooooof!",
+}
+
+console.log(animal_b);
+
+animal_b.true = "is_outdoor";
+
+console.log(animal_b)
+
+console.log(animal_b.name);
+
+console.log(animal_b.age);
